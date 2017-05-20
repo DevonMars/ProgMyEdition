13b00f9a7f144d7def3712f8c1cfc3b3b261df32
DevonMars <devon_marsham_16@live.nl>
2017-05-20 20:37:27 +0200

    postman is werkend. greyed out the orginele code in routes cities
    router.use is grey out in api1

--- practicum 3/WorldAPI/routes/api1/routes_cities.js
+++ practicum 3/WorldAPI/routes/api1/routes_cities.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var routes = express.Router();
-var mysql = require('../../mysql.js');
-
-routes.get('/city', function (req, res) {
-    res.contentType('application/json');
-
-    mysql.query('SELECT * FROM city', function (error, rows, fields) {
-        if (error){
-            res.status(400).json(error);
-        } else {
-            res.status(200).json(rows);
-        };
-
-    });
-
-});
-
-module.exports = routes;
+// var express = require('express');
+// var routes = express.Router();
+// var mysql = require('../../mysql.js');
+//
+// routes.get('/city', function (req, res) {
+//     res.contentType('application/json');
+//
+//     mysql.query('SELECT * FROM city', function (error, rows, fields) {
+//         if (error){
+//             res.status(400).json(error);
+//         } else {
+//             res.status(200).json(rows);
+//         };
+//
+//     });
+//
+// });
+//
+// module.exports = routes;
