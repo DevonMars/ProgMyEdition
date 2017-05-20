af07f96316585b23a20a894b164473d18156531f
Unknown <Thijsvanmarle@gmail.com>
2017-05-21 00:08:45 +0200

    add country added

--- practicum 3/WorldAPI/routes/api1/routes_countries.js
+++ practicum 3/WorldAPI/routes/api1/routes_countries.js
@@ -17,6 +17,24 @@ router.get("/:number", function(req, res, next) {
     })
 })
 
+router.post("/add?", function(req, res, next){
+    var name = req.get('name');
+    var code = req.get('code');
+    var continent = req.get('continent');
+    database.query(
+        'INSERT INTO country (code, name, continent) VALUES(?, ?, ?)',
+        [code, name, continent],
+        function(error, rows, fields){
+        if(error)
+            res.status(400).json(error);
+        else {
+            res.status(200);
+            res.contentType('application/json');
+            res.json({"msg" : "country succesfully added"});
+        }
+    })
+})
+
 router.get("*", function(req, res, next){
   database.query('SELECT * FROM country;', function(error, rows, fields){
       if(error)
