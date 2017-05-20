e7afe7425bfe0ddfabcc7c1b7467afe15d207fee
Unknown <Thijsvanmarle@gmail.com>
2017-05-21 00:55:32 +0200

    setup for search function

--- practicum 3/WorldAPI/routes/routes_api_v1.js
+++ practicum 3/WorldAPI/routes/routes_api_v1.js
@@ -2,11 +2,30 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var config = require('../config');
-var mysql = require('../mysql')
+var database = require('../mysql')
 
 router.use("/countries", require('./api1/routes_countries'));
-router.use('/city', require('./api1/routes_cities'));
+router.use("/city", require('./api1/routes_cities'));
 
-
+router.get("/search?", function(req, res, next){
+    var type = req.query.type;
+    var continent = req.query.continent;
+    var amount = parseInt(req.query.amount);
+    database.query(
+        "SELECT * FROM country WHERE continent=?",
+        [continent],
+        function(error, rows, fields) {
+            if (error)
+                res.status(400).json(error);
+            else{
+                res.status(200);
+                res.contentType("application/json");
+                for(var i = 0; i < amount; i++) {
+                    res.json(rows[i]);
+                }
+            }
+        }
+    )
+})
 
 module.exports = router;
